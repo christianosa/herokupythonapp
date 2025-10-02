f94face14bd47d0f981affa2af0c35b1b6944b1c
Christiano Sá <christiano.sa@gmail.com>
2025-10-01 23:12:21 -0300

    "Ajuste referência modulos"

--- source/scrapping.py
+++ source/scrapping.py
@@ -11,7 +11,7 @@ import sqlite3
 import sys
 import os
 
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # from modules.utils.livro_dao import LivroDAO
 # from modules.utils.functions import trataTitulo, trataPreco, obterObjetos, obterTotal, trataDisponibilidade
